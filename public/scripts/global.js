b2762d352016f7f081141844b763c49a127b4ad7
blockheron <64575353+blockheron@users.noreply.github.com>
2024-12-10 12:12:11 -0500

    404 pages, new stories

---
+++ public/scripts/global.js
@@ -0,0 +1,27 @@
+var latestStory = 4;
+var storyURL = "https://sparrowflightproject.web.app/stories/story-";
+
+function storyButtons(index) {
+    const pageIndices = document.getElementsByName("pageIndex");
+    for (var i = 0; i < 2; i++) {
+        pageIndices[i].innerHTML = index;
+    }
+    
+    if (index < latestStory) {
+        const nextButtons = document.getElementsByName("nextButton");
+        const lastButtons = document.getElementsByName("lastButton");
+        for (var i = 0; i < 2; i++) {
+            nextButtons[i].setAttribute("href", storyURL + (index+1) + ".html");
+            lastButtons[i].setAttribute("href", storyURL + latestStory + ".html");
+        }
+    }
+
+    if (index != 0) {
+        const firstButtons = document.getElementsByName("firstButton");
+        const prevButtons = document.getElementsByName("prevButton");
+        for (var i = 0; i < 2; i++) {
+            firstButtons[i].setAttribute("href", storyURL + 0 + ".html");
+            prevButtons[i].setAttribute("href", storyURL + (index-1) + ".html");
+        }
+    }
+}
