c8f21928a021a3276a5c2aae79304ea05393534a
blockheron <64575353+blockheron@users.noreply.github.com>
2025-10-19 21:22:55 -0400

    Light/dark mode

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -31,3 +31,74 @@ function setLatestStory() {
     const latestStoryLink = document.getElementsByName("latestStoryButton");
     latestStoryLink[0].setAttribute("href", "stories/story-" + latestStory + ".html");
 }
+
+// dark, light, dyslexia themes
+let light = localStorage.getItem("light");
+let lightDys = localStorage.getItem("light-dys");
+let darkDys = localStorage.getItem("dark-dys");
+
+const enableLightMode = () => {
+    document.body.classList.add("light");
+    localStorage.setItem("light", "active")
+}
+
+const enableLightDysMode = () => {
+    document.body.classList.remove("light");
+    localStorage.setItem("light", null)
+    document.body.classList.add("light-dys");
+    localStorage.setItem("light-dys", "active")
+}
+
+const enableDarkDysMode = () => {
+    document.body.classList.remove("light-dys");
+    localStorage.setItem("light-dys", null)
+    document.body.classList.add("dark-dys");
+    localStorage.setItem("dark-dys", "active")
+}
+
+const enableDarkMode = () => {
+    document.body.classList.remove("dark-dys");
+    localStorage.setItem("dark-dys", null)
+}
+
+if (light === "active") enableLightMode();
+if (lightDys === "active") enableLightDysMode();
+if (darkDys === "active") enableDarkDysMode();
+
+function changeTheme() {
+    light = localStorage.getItem("light");
+    lightDys = localStorage.getItem("light-dys");
+    darkDys = localStorage.getItem("dark-dys");
+
+    if (light === "active") {
+        console.log("lightDys");
+        enableLightDysMode();
+    } else if (lightDys === "active") {
+        console.log("darkDys");
+        enableDarkDysMode();
+    } else if (darkDys === "active") {
+        console.log("dark");
+        enableDarkMode();
+    } else {
+        enableLightMode();
+        console.log("light");
+    }
+
+    changeThemeIframe();
+}
+
+function changeThemeIframe() {
+    if (light === "active") {
+        console.log("lightDys");
+        enableLightDysMode();
+    } else if (lightDys === "active") {
+        console.log("darkDys");
+        enableDarkDysMode();
+    } else if (darkDys === "active") {
+        console.log("dark");
+        enableDarkMode();
+    } else {
+        enableLightMode();
+        console.log("light");
+    }
+}
